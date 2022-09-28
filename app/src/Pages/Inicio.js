f2b581851d1acf08a76852b17c25c54be577099d
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2022-09-28 00:18:07 -0400

    🧪

--- app/src/Pages/Inicio.js
+++ app/src/Pages/Inicio.js
@@ -1,10 +1,50 @@
 import React from 'react';
-import { SHr, SIcon, SPage, SText, STheme, SView } from "servisofts-component";
+
+import { SButtom, SHr, SPage, SText, SView } from "servisofts-component";
 
 import PBarraFooter from "../Components/PBarraFooter";
 
 
 export default class Inicio extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      saludar:"hola a todos"
+    };
+  }
+
+  componentDidMount() {
+    this.vuelos();
+  }
+
+
+  async vuelos() {
+    const api01 = await fetch("http://67.205.155.238/api/vuelo", { method: 'GET' });
+    const dataLibros = await api01.json();
+
+    // verifico conexion con la api
+    if (!api01) {
+      const message = `An error has occured: ${api01.status}`;
+      throw new Error(message);
+    }
+
+    let html = ``;
+    Object.keys(dataLibros).map((key) => {
+      var obj = dataLibros[key];
+      html += "key "+ JSON.stringify(obj.key)+"\n";
+    });
+
+    this.setState({saludar : html})
+    // imprimo los datos
+    // html += `</br> `;
+    // return html;
+  }
+
+
+
+
+
   render() {
     return (
       <>
@@ -14,13 +54,17 @@ export default class Inicio extends React.Component {
             <SHr height={25} />
             <SText >Inicio</SText>
           </SView>
-
           <SView flex center>
             <SView col={"xs-5"} height={200}>
-              <SIcon name={"Logo"} fill={STheme.color.primary} />
+
+              {/* <SButtom props={{ type: "danger" }} onPress={() => {
+                this.vuelos()
+              }}>Logout</SButtom> */}
+
+
+              <SText>{this.state.saludar}</SText>
             </SView>
           </SView>
-
         </SPage>
 
         <SHr height={20} />
