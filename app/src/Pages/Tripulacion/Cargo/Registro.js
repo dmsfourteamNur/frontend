017439bb99296ecb7670ccde2dcf1c2f629369d1
Licethd <liceth.dfe@gmail.com>
2022-10-05 18:25:07 -0400

    f5

--- app/src/Pages/Tripulacion/Cargo/Registro.js
+++ app/src/Pages/Tripulacion/Cargo/Registro.js
@@ -1,39 +1,43 @@
 import { useEffect, useState, useRef } from 'react';
-import { SButtom, SForm, SHr, SIcon, SPage, SText, STheme, SView, STable2 } from 'servisofts-component';
-import FloatButtom from '../../../Components/FloatButtom';
-
-
-
+import { SButtom, SForm, SHr, SIcon, SPage, SText, STheme, SView, STable2, SNavigation, SLoad } from 'servisofts-component';
+import Config from '../../../config.json'
 
 export default (props) => {
 
     const formulario = useRef();
 
+    var keyEdit = "";
+    if (SNavigation.getParam('key')) {
+        keyEdit = SNavigation.getParam('key');
+    }
+
     const [state, setState] = useState({
-        data: []
+        data: [],
+        key: keyEdit
     });
 
+    if (state.key != "") {
 
-    // useEffect(() => {
-    //     var requestOptions = {
-    //         method: 'GET',
-    //         redirect: 'follow'
-    //     };
+        useEffect(() => {
+            var requestOptions = {
+                method: 'GET',
+                redirect: 'follow'
+            };
 
-    //     fetch("http://localhost:8080/tripulante", requestOptions)
-    //         // .then(response => response.text())
-    //         .then(response => response.json())
-    //         .then(result => {
-    //             state.data = result;
-    //             setState({ ...state })
-    //             // var obj = JSON.parse(state.data )
-    //             console.log(state.data)
-    //         })
-    //         .catch(error => console.log('error', error));
-    // }, [])
+            fetch(Config.SERVER_URL_TRIPULACION + "cargo/" + state.key, requestOptions)
+                .then(response => response.json())
+                .then(result => {
+                    state.data = result;
+                    setState({ ...state })
+                    console.log(state.data)
+                })
+                .catch(error => console.log('error', error));
+        }, [])
 
+        if (!state?.data.Descripcion) return <SLoad />
 
-    let data = {};
+    }
+
     return (
         <>
             <SPage title={'Registro Cargo'}>
@@ -47,9 +51,7 @@ export default (props) => {
                             center
                             row
                             ref={(form) => {
-                                // this.form = form;
                                 formulario.current = form;
-
                             }}
                             style={{
                                 justifyContent: 'space-between',
@@ -67,44 +69,39 @@ export default (props) => {
                                     label: 'Descripción',
                                     type: 'text',
                                     isRequired: true,
-                                    // defaultValue: data['descripcion']
+                                    defaultValue: state.data?.Descripcion
                                 },
 
                             }}
                             // onSubmitName={"Registrar"}
                             onSubmit={(values) => {
-                                // if (this.key) {
-                                //     sector.Actions.editar({ ...data, ...values }, this.props);
-                                // } else {
-                                //     sector.Actions.registro(
-                                //         { ...values, key_evento: this.key_evento },
-                                //         this.props
-                                //     );
-                                var myHeaders = new Headers();
-                                // myHeaders.
-                                // myHeaders.append("Access-Control-Allow-Origin", "http://localhost:8080/cargo/registro")
-                                // myHeaders.append("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
-                                // myHeaders.append("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
-                                myHeaders.append("Content-Type", "application/json");
-
                                 var raw = JSON.stringify({
-                                    // "Nombre": "Dana Test2",
-                                    // "Apellido": "Portillo Test2"
                                     ...values
                                 });
 
-                                var requestOptions = {
-                                    method: 'POST',
-                                    //headers: myHeaders,
-                                    body: raw,
-                                   // redirect: 'follow'
-                                };
+                                if (state.key != "") {
+                                    var requestOptions = {
+                                        method: 'PUT',
+                                        body: raw,
+                                    };
 
-                                fetch("http://localhost:8080/cargo/registro", requestOptions)
-                                    .then(response => response.json())
-                                    .then(result => console.log(result))
-                                    .catch(error => console.log('error', error));
-                                // }
+                                    fetch(Config.SERVER_URL_TRIPULACION + "cargo/" + state.key, requestOptions)
+                                        .then(response => response.json())
+                                        .then(result => console.log(result))
+                                        .then(result => SNavigation.goBack())
+                                        .catch(error => console.log('error', error));
+                                } else {
+                                    var requestOptions = {
+                                        method: 'POST',
+                                        body: raw,
+                                    };
+
+                                    fetch(Config.SERVER_URL_TRIPULACION + "cargo/registro", requestOptions)
+                                        .then(response => response.json())
+                                        .then(result => console.log(result))
+                                        .then(result => SNavigation.goBack())
+                                        .catch(error => console.log('error', error));
+                                }
                             }}
                         />
                     </SView>
@@ -122,7 +119,7 @@ export default (props) => {
                         formulario.current.submit();
                     }}>
                     <SText color={STheme.color.text} font={'Roboto'} fontSize={14} bold>
-                        REGISTRAR
+                        {state.key ? 'EDITAR' : 'REGISTRAR'}
                     </SText>
                 </SView>
             </SView>
