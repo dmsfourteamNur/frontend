daaa654a080cfeb89131890d7980bf3fa184f4b9
rickypazd <rickypazd@hotmail.com>
2022-10-10 20:45:40 -0400

    Update root.js

--- app/src/Pages/root.js
+++ app/src/Pages/root.js
@@ -1,12 +1,11 @@
-import { useEffect, useState } from 'react';
-import { SButtom, SForm, SHr, SIcon, SPage, SText, STheme, SView, STable2, SNavigation } from 'servisofts-component';
+import { SPage, SView } from 'servisofts-component';
 import ButtonCuadrado from '../Components/ButtonCuadrado';
 
 export default (props) => {
 	return (
 		<SPage title={'Home'} disableScroll>
 			<SView center col={'xs-12'}  >
-				<SView col={'xs-11'} center row>
+				<SView col={'xs-12'} center row>
 					<ButtonCuadrado url="/tripulacion" name="Tripulacion" icon="Ajustes" />
 					<ButtonCuadrado url="/aeronave" name="Aeronave" icon="Ajustes" />
 					<ButtonCuadrado url="/vuelo" name="Vuelo" icon="Ajustes" />
