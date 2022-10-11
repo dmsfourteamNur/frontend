6c17425061334ed42f781fe49c43f9152e8f5750
rickypazd <rickypazd@hotmail.com>
2022-10-10 20:37:30 -0400

    asd

--- app/src/Pages/aeronave/marca/index.ts
+++ app/src/Pages/aeronave/marca/index.ts
@@ -3,6 +3,6 @@ import { SPage } from "servisofts-component";
 import root from "./root";
 import registro from "./registro";
 export default SPage.combinePages("marca", {
-    "": root,
-    "registro": registro
+	"": root,
+	registro
 });
