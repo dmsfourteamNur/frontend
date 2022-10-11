7b2221e5944bd2911766be9a45a53c245f7a8950
rickypazd <rickypazd@hotmail.com>
2022-10-10 20:43:01 -0400

    asd

--- app/src/Pages/root.js
+++ app/src/Pages/root.js
@@ -1,110 +1,20 @@
-import {
-  SButtom,
-  SHr,
-  SIcon,
-  SNavigation,
-  SPage,
-  SText,
-  STheme,
-  SView,
-} from "servisofts-component";
+import { useEffect, useState } from 'react';
+import { SButtom, SForm, SHr, SIcon, SPage, SText, STheme, SView, STable2, SNavigation } from 'servisofts-component';
+import ButtonCuadrado from '../Components/ButtonCuadrado';
 
 export default (props) => {
-  return (
-    <SPage hidden disableScroll center>
-      <SView
-        col={"xs-9 sm-7 md-5 lg-12 xl-12"}
-        height={200}
-        row
-        center
-        backgroundColor={"transparent"}
-      >
-        <SView
-          width={100}
-          height={100}
-          backgroundColor={"#20a6d8"}
-          center
-          row
-          style={{ borderRadius: 8 }}
-          onPress={() => {
-            SNavigation.navigate("/aeronave");
-          }}
-        >
-          <SText center col={"xs-12"}>
-            Microservicio Aeronave
-          </SText>
-        </SView>
-        <SView width={10} />
-
-        <SView
-          width={100}
-          height={100}
-          backgroundColor={"#e292d2"}
-          center
-          row
-          style={{ borderRadius: 8 }}
-          onPress={() => {
-            SNavigation.navigate("/tripulacion");
-          }}
-        >
-          <SText center col={"xs-12"}>
-            Microservicio Tripulación
-          </SText>
-        </SView>
-        <SView width={10} />
-
-        <SView
-          width={100}
-          height={100}
-          backgroundColor={"#877771"}
-          center
-          row
-          style={{ borderRadius: 8 }}
-          onPress={() => {
-            SNavigation.navigate("/vuelo/inicio");
-          }}
-        >
-          <SText center col={"xs-12"}>
-            Microservicio Vuelo
-          </SText>
-        </SView>
-        <SView width={10} />
-
-        <SView
-          width={100}
-          height={100}
-          style={{ borderRadius: 8 }}
-          backgroundColor={"#ffc942"}
-          center
-          row
-          onPress={() => {
-            SNavigation.navigate("/venta/inicio");
-          }}
-        >
-          <SText center col={"xs-12"}>
-            Microservicio Venta
-          </SText>
-        </SView>
-        <SView width={10} />
-
-        <SView
-          width={100}
-          height={100}
-          backgroundColor={"#7d3b23"}
-          center
-          style={{ borderRadius: 8 }}
-          row
-          onPress={() => {
-            SNavigation.navigate("/checking/inicio");
-          }}
-        >
-          <SText center col={"xs-12"}>
-            Microservicio Checking
-          </SText>
-        </SView>
-        <SView width={10} />
-      </SView>
-      <SHr />
-    </SPage>
-  );
-};
+	return (
+		<SPage title={'Aeronave'} disableScroll>
+			<SView center col={'xs-12'} height >
+				<SHr height={50} />
+				<SView col={'xs-11 md-10 xl-10'} center row>
+					<ButtonCuadrado url="/tripulacion" name="Tripulacion" icon="Ajustes" />
+					<ButtonCuadrado url="/aeronave" name="Aeronave" icon="Ajustes" />
+					<ButtonCuadrado url="/vuelo" name="Vuelo" icon="Ajustes" />
+					<ButtonCuadrado url="/venta" name="Venta" icon="Ajustes" />
+					<ButtonCuadrado url="/checkIn" name="CheckIn" icon="Ajustes" />
+				</SView>
+			</SView>
+		</SPage>
+	);
+}
