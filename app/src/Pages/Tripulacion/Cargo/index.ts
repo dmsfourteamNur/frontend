1f436b78b6ec0f95a7d3531bcd898c405f4c2341
rickypazd <rickypazd@hotmail.com>
2022-10-10 20:20:57 -0400

    as

--- app/src/Pages/Tripulacion/Cargo/index.ts
+++ app/src/Pages/Tripulacion/Cargo/index.ts
@@ -1,5 +1,5 @@
 import { SPage } from "servisofts-component";
-import Registro from "./Registro";
+import Registro from "./registro";
 import root from "./root";
  export default SPage.combinePages("cargo", {
     "": root,
