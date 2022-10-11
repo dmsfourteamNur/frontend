48375ee466a202775a39c148dd3bb20fe6dc735e
rickypazd <rickypazd@hotmail.com>
2022-10-10 20:44:14 -0400

    Update root.js

--- app/src/Pages/root.js
+++ app/src/Pages/root.js
@@ -4,10 +4,9 @@ import ButtonCuadrado from '../Components/ButtonCuadrado';
 
 export default (props) => {
 	return (
-		<SPage title={'Aeronave'} disableScroll>
-			<SView center col={'xs-12'} height >
-				<SHr height={50} />
-				<SView col={'xs-11 md-10 xl-10'} center row>
+		<SPage title={'Home'} disableScroll>
+			<SView center col={'xs-12'}  >
+				<SView col={'xs-11'} center row>
 					<ButtonCuadrado url="/tripulacion" name="Tripulacion" icon="Ajustes" />
 					<ButtonCuadrado url="/aeronave" name="Aeronave" icon="Ajustes" />
 					<ButtonCuadrado url="/vuelo" name="Vuelo" icon="Ajustes" />
