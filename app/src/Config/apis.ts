eee3400c0cfbc2ebb72b95a8f8edfc43c7846506
rickypazd <rickypazd@hotmail.com>
2022-10-10 20:35:26 -0400

    asd

--- app/src/Config/apis.ts
+++ app/src/Config/apis.ts
@@ -1,5 +1,8 @@
 export default {
-    "tripulacion": "http://192.168.3.3:8080/",
-    "aeronave": "http://127.0.0.1:8080/",
+	"tripulacion": "http://192.168.3.3:8080/",
+	"aeronave": "http://127.0.0.1:8080/",
+	"vuelo": "http://127.0.0.1:8080/",
+	"venta": "http://127.0.0.1:8080/",
+	"checkIn": "http://127.0.0.1:8080/",
 
 }
--- app/src/Pages/index.ts
+++ app/src/Pages/index.ts
@@ -1,17 +1,14 @@
 import { SPage } from "servisofts-component";
 import login from "./login";
 import root from "./root";
-import test from "./test";
 
 import aeronave from './aeronave';
 import msVuelo from "./msVuelo/index";
 import Tripulacion from "./Tripulacion";
 export default SPage.combinePages("/", {
-  "": root,
-  // "": tripulacionInicio,
-  login: login,
-  test: test,
-  ...aeronave,
+	"": root,
+	login: login,
+	...aeronave,
 	...Tripulacion,
 	...msVuelo,
 });
