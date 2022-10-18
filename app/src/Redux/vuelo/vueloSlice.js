7ae4587232f1a31eb1f9fb17cde8c5f1d471394d
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2022-10-18 09:48:10 -0400

    ok
    ok

--- app/src/Redux/vuelo/vueloSlice.js
+++ app/src/Redux/vuelo/vueloSlice.js
@@ -23,35 +23,15 @@ const Slice = createSlice({
 
 export const getAll = createAsyncThunk(name + '/getAll', API.getAll);
 const getAll_ = (builder) => {
-	builder.addCase(getAll.pending, (state, action) => {
-		state.loading = true;
-	});
-	builder.addCase(getAll.fulfilled, (state, action) => {
-		state.loading = false;
-		state.data = {};
-		action.payload.map(obj => {
-			state.data[obj.key] = obj;
-		})
-	});
-	builder.addCase(getAll.rejected, (state, action) => {
-		state.loading = false;
-		state.error = action.payload;
-	});
+	builder.addCase(getAll.pending, (state, action) => { state.loading = true; });
+	builder.addCase(getAll.fulfilled, (state, action) => { state.loading = false; state.data = {}; action.payload.map(obj => { state.data[obj.key] = obj; }) });
+	builder.addCase(getAll.rejected, (state, action) => { state.loading = false; state.error = action.payload; });
 }
 export const getByKey = createAsyncThunk(name + '/getByKey', API.getByKey);
 const getByKey_ = (builder) => {
-	builder.addCase(getByKey.pending, (state, action) => {
-		state.loading = true;
-	});
-	builder.addCase(getByKey.fulfilled, (state, action) => {
-		state.loading = false;
-		state.data[action.payload.key] = action.payload;
-	});
-	builder.addCase(getByKey.rejected, (state, action) => {
-		state.loading = false;
-		state.error = action.payload;
-	});
-
+	builder.addCase(getByKey.pending, (state, action) => { state.loading = true; });
+	builder.addCase(getByKey.fulfilled, (state, action) => { state.loading = false; state.data[action.payload.key] = action.payload; });
+	builder.addCase(getByKey.rejected, (state, action) => { state.loading = false; state.error = action.payload; });
 }
 export const remove = createAsyncThunk(name + '/delete', API.remove);
 const remove_ = (builder) => {
