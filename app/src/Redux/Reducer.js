9bc8078cac43a1a5372b0c22173104b02a1df98b
edsoncito <edson.nur@gmail.com>
2022-10-21 03:40:11 -0400

    addd

--- app/src/Redux/Reducer.js
+++ app/src/Redux/Reducer.js
@@ -1,23 +1,25 @@
-import checkInReducer from "./checkIn/checkinSlice";
+import checkInReducer from './checkIn/checkinSlice';
 
-import aeronaveSlice from "./aeronave/aeronaveSlice";
-import marcaSlice from "./aeronave/marcaSlice";
-import ventaSlice from "./venta/ventaSlice";
-import cargonReducer from "./tripulacion/cargoSlice";
-import tripulacionReducer from "./tripulacion/tripulacionSlice";
-import tripulanteReducer from "./tripulacion/tripulanteSlice";
-import aeronavesReducer from "./vuelo/aeronaveSlice";
-import tripulacionesReducer from "./vuelo/tripulacionSlice";
-import vueloReducer from "./vuelo/vueloSlice";
+import aeronaveSlice from './aeronave/aeronaveSlice';
+import marcaSlice from './aeronave/marcaSlice';
+import ventaSlice from './venta/ventaSlice';
+import cargonReducer from './tripulacion/cargoSlice';
+import tripulacionReducer from './tripulacion/tripulacionSlice';
+import tripulanteReducer from './tripulacion/tripulanteSlice';
+import aeronavesReducer from './vuelo/aeronaveSlice';
+import tripulacionesReducer from './vuelo/tripulacionSlice';
+import vueloReducer from './vuelo/vueloSlice';
+import personaByIdSlice from './checkIn/personaByIdSlice';
 export default {
-	checkin: checkInReducer,
-	tripulante: tripulanteReducer,
-	tripulacion: tripulacionReducer,
-	cargo: cargonReducer,
-	vuelo: vueloReducer,
-	aeronaves: aeronavesReducer,
-	tripulaciones: tripulacionesReducer,
-	aeronave: aeronaveSlice,
-	marca: marcaSlice,
-	venta: ventaSlice
+  checkin: checkInReducer,
+  tripulante: tripulanteReducer,
+  tripulacion: tripulacionReducer,
+  cargo: cargonReducer,
+  vuelo: vueloReducer,
+  aeronaves: aeronavesReducer,
+  tripulaciones: tripulacionesReducer,
+  aeronave: aeronaveSlice,
+  marca: marcaSlice,
+  venta: ventaSlice,
+  persona: personaByIdSlice
 };
--- app/src/services/checkin/checkInApi.js
+++ app/src/services/checkin/checkInApi.js
@@ -1,7 +1,7 @@
 import Api from './Api';
 
-const getAll = async (data) => {
-  const response = await Api().get('checkin', data, {timeout: 8000});
+const getAll = async () => {
+  const response = await Api().get('checkin', {timeout: 8000});
   return response.data;
 };
 
