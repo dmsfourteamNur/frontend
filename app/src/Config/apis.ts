3b43f565f38a2a5bf685c51269cee3064a6c4a80
rickypazd <rickypazd@hotmail.com>
2022-10-22 01:04:01 -0400

    Update apis.ts

--- app/src/Config/apis.ts
+++ app/src/Config/apis.ts
@@ -5,11 +5,3 @@ export default {
 	"venta": "http://35.229.87.198/api/",
 	"checkIn": "http://34.139.73.131/api/",
 }
-// export default {
-// 	"tripulacion": "http://34.73.149.43/api/",
-// 	"aeronave": "http://34.125.212.163/api/",
-// 	"vuelo": "http://127.0.0.1:8080/api/",
-// 	"venta": "http://35.229.87.198/api/",
-// 	"checkIn": "http://127.0.0.1:8080/",
-
-// }
